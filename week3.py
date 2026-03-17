844b8061050acbda918e19e41e2e976c5c9f5418
mababajo <mababajo@gmail.com>
2026-03-17 19:37:37 +0100

    Add files via upload

---
+++ week3.py
@@ -0,0 +1,17 @@
+test_bank = [
+    {
+        "question": "What is the capital of Nigeria?",
+        "options": ["A. Lagos", "B. Abuja", "C. Kaduna"],
+        "correct_answer": "B"
+    },
+    {
+        "question": "What is 3 + 3?",
+        "options": ["A. 1", "B. 6", "C. 4"],
+        "correct_answer": "B"
+    },
+    {
+        "question": "Who is the GOAT?",
+        "options": ["A. Ronaldo", "B. Pele", "C. Messi"],
+        "correct_answer": "C"
+    }
+]
